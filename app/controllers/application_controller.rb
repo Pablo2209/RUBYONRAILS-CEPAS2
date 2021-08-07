f21be6851a742f72e2adc579f360f95dde606c79
Pablo <pablochavezing@gmail.com>
2021-08-07 15:00:26 -0700

    Update security with devise2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::Base
-    /Metodo para definir un usuario editor/
+    /Metodo para definir un usuario editor
+    
     def authorize_editor!
         unless current_user.editor?
             flash[:error] = "You must be a editor in to access this section"
             redirect_to new_user_session_url
         end
-    end
+    end/
 end
