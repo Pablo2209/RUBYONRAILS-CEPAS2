0ed447f061e15d918c866a55a1f0adf200f3dd17
Pablo <pablochavezing@gmail.com>
2021-08-10 17:16:45 -0700

    Add method delete to devise

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :strains
   resources :wines
   devise_for :users, :controller => { registrations: 'registration'}
-
+  
   root 'wines#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
